d94876ec39be9d869b73ce5f2929642be4b85b1b
GitHub Action <action@github.com>
2024-03-28 14:08:27 +0000

    Update colorexp formula to version v1.0.5

--- Formula/colorexp.rb
+++ Formula/colorexp.rb
@@ -1,8 +1,8 @@
 class Colorexp < Formula
   desc "colorexp: Command line multi-color regexp highlighter"
   homepage "https://github.com/EugenDueck/colorexp"
-  url "https://github.com/EugenDueck/colorexp/releases/download/v1.0.4/colorexp-darwin-amd64-1.0.4-46ae94a.tar.gz"
-  sha256 "78cb222e97d2e49f3268ab8873c0815cfd2f9c22005831dc69ce7c3ac526114a"
+  url "https://github.com/EugenDueck/colorexp/releases/download/v1.0.5/colorexp-darwin-amd64-1.0.5-d77a35a.tar.gz"
+  sha256 "83470d7e5143b3d4e9e9b7ab235e9f7354e64ee850b41926a5655ae6bc0113b8"
 
   def install
     bin.install "colorexp"
