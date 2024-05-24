87f69ea08d4ccfb640a5ebb5bd1b742464fbcbc4
EugenDueck <eugen@dueck.org>
2024-05-25 06:08:38 +0900

    Add "version" to colorexp.rb

--- Formula/colorexp.rb
+++ Formula/colorexp.rb
@@ -3,6 +3,7 @@ class Colorexp < Formula
   homepage "https://github.com/EugenDueck/colorexp"
   url "https://github.com/EugenDueck/colorexp/releases/download/v2/colorexp-darwin-amd64-2-acbf88e.tar.gz"
   sha256 "97ef4c04a722bcacb1f70e5dc0e9fec66cb64df39a780d8e28b4a678b0d238ba"
+  version "2"
 
   def install
     bin.install "colorexp"
